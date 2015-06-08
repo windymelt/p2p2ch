99a717ca54358db9a1fce9892f222f9a65007cd3
hiroyuki nagata <newserver002@gmail.com>
2015-06-08 23:51:51 +0900

    とりあえずビルドを修正

--- project/build.scala
+++ project/build.scala
@@ -8,21 +8,30 @@ import org.scalastyle.sbt.ScalastylePlugin
 object P2PScalaProto extends Build {
 
   lazy val P2PScalaProto =
-    ProjectRef(new URI("https://github.com/windymelt/p2pScalaProto.git"), "P2PScalaProto")
+    ProjectRef(new URI("https://github.com/Hiroyuki-Nagata/p2pScalaProto.git"), "P2PScalaProto")
 
-  val Organization = "momijikawa"
-  val Name = "P2P2ch"
-  val Version = "1.10.0" 
-  val ScalaVersion = "2.10.2" // Scalaバージョンは固定する
+  val Organization  = "momijikawa"
+  val Name          = "P2P2ch"
+  val Version       = "1.10.0" 
+  val ScalaVersion  = "2.10.4"
+  val ScalazVersion = "7.0.0"
+  val AkkaVersion   = "2.2.3"
 
   // << groupId >> %%  << artifactId >> % << version >>
   lazy val LibraryDependencies = Seq(
-    "com.typesafe.akka" %% "akka-actor" % "2.2.3",
-    "com.typesafe.akka" %% "akka-remote" % "2.2.3",
-    "com.typesafe.akka" %% "akka-agent" % "2.2.3",
+    "com.typesafe.akka" %% "akka-actor"  % AkkaVersion,
+    "com.typesafe.akka" %% "akka-remote" % AkkaVersion,
+    "com.typesafe.akka" %% "akka-agent"  % AkkaVersion,
+    "org.scalaz" %% "scalaz-core"               % ScalazVersion,
+    "org.scalaz" %% "scalaz-effect"             % ScalazVersion,
+    "org.scalaz" %% "scalaz-typelevel"          % ScalazVersion,
+    "org.scalaz" %% "scalaz-scalacheck-binding" % ScalazVersion % "test",
     jdbc,
     anorm,
     cache % "test",
+    "org.specs2" %% "specs2-core" % "3.1" % "test",
+    "org.mockito" % "mockito-all" % "1.9.5",
+    "commons-codec" % "commons-codec" % "1.9",
     "net.sourceforge.htmlunit" % "htmlunit" % "2.14" % "test",
     "org.apache.httpcomponents" % "httpclient" % "4.4",
     "org.apache.httpcomponents" % "httpcore" % "4.4" % "test"
@@ -39,6 +48,7 @@ object P2PScalaProto extends Build {
     resolvers += JavaNet1Repository,
     resolvers += Classpaths.typesafeReleases,
     resolvers += "Momijikawa Maven repository on GitHub" at "http://windymelt.github.io/repo/",
+    resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases",
     libraryDependencies ++= LibraryDependencies
   )
 
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,18 +1,8 @@
 // Comment to get more information during initialization
 logLevel := Level.Warn
 
-// The Typesafe repository 
-resolvers ++= Seq(
-Resolver.file("Local Repository", file("<projdir>/Play20/repository/local"))(Resolver.ivyStylePatterns),
-"Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
-)
-
 resolvers += "sonatype-releases" at "https://oss.sonatype.org/content/repositories/releases/"
 
-resolvers += Resolver.url("artifactory", url("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns)
-
-
-// Use the Play sbt plugin for Play projects
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.2.1")
 
 addSbtPlugin("org.ensime" % "ensime-sbt-cmd" % "0.1.2")
