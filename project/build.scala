bfd011882821e221e2f21f0b89f982ff6b9a491f
hiroyuki nagata <newserver002@gmail.com>
2015-06-11 01:48:25 +0900

    以下のバージョンで統一
    ScalaVersion  = "2.10.4"
    ScalazVersion = "7.1.1"
    AkkaVersion   = "2.2.3"
    Specs2Version = "3.1"
    
    play-testが古いバージョンのSpecs2を引っ張ってくる不具合を修正

--- project/build.scala
+++ project/build.scala
@@ -1,14 +1,24 @@
+import com.typesafe.sbt.SbtScalariform
+import com.typesafe.sbt.SbtScalariform.ScalariformKeys
+import com.typesafe.sbt.SbtScalariform.scalariformSettings
+import java.net.URI
+import org.scalastyle.sbt.ScalastylePlugin
 import play.Project._
 import sbt.Keys._
 import sbt._
-import java.net.URI
-import com.typesafe.sbt.SbtScalariform.scalariformSettings
-import org.scalastyle.sbt.ScalastylePlugin
+import scalariform.formatter.preferences._
 
 object P2P2ch extends Build {
 
   lazy val P2PScalaProto =
     ProjectRef(new URI("https://github.com/windymelt/p2pScalaProto.git"), "P2PScalaProto")
+
+  ScalariformKeys.preferences := ScalariformKeys.preferences.value
+    .setPreference(RewriteArrowSymbols, true)
+    .setPreference(AlignSingleLineCaseStatements, true)
+    .setPreference(AlignParameters, true)
+    .setPreference(DoubleIndentClassDeclaration, true)
+    .setPreference(SpacesWithinPatternBinders, true)
 
   val Organization  = "momijikawa"
   val Name          = "P2P2ch"
@@ -16,6 +26,7 @@ object P2P2ch extends Build {
   val ScalaVersion  = "2.10.4"
   val ScalazVersion = "7.1.1"
   val AkkaVersion   = "2.2.3"
+  val Specs2Version = "3.1"
 
   // << groupId >> %%  << artifactId >> % << version >>
   lazy val LibraryDependencies = Seq(
@@ -29,7 +40,17 @@ object P2P2ch extends Build {
     jdbc,
     anorm,
     cache % "test",
-    "org.specs2" %% "specs2-core" % "3.1" % "test",
+    // Scalaのバイナリ互換性の無さには吐き気がする
+    // http://d.hatena.ne.jp/xuwei/20140825/1408979619
+    // play-testがSpecs2 2.1.1を引っ張ってくるのでここで止める
+    "com.typesafe.play" %% "play-test" % "2.2.1" % "test" excludeAll(
+      ExclusionRule(organization = "org.specs2")
+    ),
+    // Specs2の2.4.0以降はscalaz7.1.xに依存
+    // 上でScalaz 7.1.1を指定しているのでそれはOK
+    "org.specs2" %% "specs2-core"       % Specs2Version % "test",
+    "org.specs2" %% "specs2-mock"       % Specs2Version % "test",
+    "org.specs2" %% "specs2-scalacheck" % Specs2Version % "test",
     "org.mockito" % "mockito-all" % "1.9.5",
     "commons-codec" % "commons-codec" % "1.9",
     "net.sourceforge.htmlunit" % "htmlunit" % "2.14" % "test",
@@ -47,8 +68,9 @@ object P2P2ch extends Build {
     resolvers += DefaultMavenRepository,
     resolvers += JavaNet1Repository,
     resolvers += Classpaths.typesafeReleases,
+    resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
     resolvers += "Momijikawa Maven repository on GitHub" at "http://windymelt.github.io/repo/",
-    resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases",
+    resolvers += "Scalaz-bintray" at "http://dl.bintray.com/scalaz/releases",
     libraryDependencies ++= LibraryDependencies
   )
 
@@ -57,7 +79,8 @@ object P2P2ch extends Build {
     file("."),
     settings =
       Defaults.defaultSettings ++
-        playScalaSettings ++
+        playScalaSettings      ++
+        scalariformSettings    ++
         projectSettings
   ) dependsOn (
     P2PScalaProto
